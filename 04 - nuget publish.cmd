c436b0348d24b30f3b863f893ebaed2dc2f57283
Alexander Moerman <alexander.moerman@gmail.com>
2018-04-02 16:01:27 +0200

    Change nuget publish script to 4.0.0

--- 04 - nuget publish.cmd
+++ 04 - nuget publish.cmd
@@ -1,3 +1,3 @@
-dotnet nuget push "HtmlBuilders/bin/Release/HtmlBuilders.3.0.1.nupkg" -s https://api.nuget.org/v3/index.json
-dotnet nuget push "HtmlBuilders/bin/Release/HtmlBuilders.3.0.1.symbols.nupkg" -s https://nuget.smbsrc.net/
+dotnet nuget push "HtmlBuilders/bin/Release/HtmlBuilders.4.0.0.nupkg" -s https://api.nuget.org/v3/index.json
+dotnet nuget push "HtmlBuilders/bin/Release/HtmlBuilders.4.0.0.symbols.nupkg" -s https://nuget.smbsrc.net/
 pause
