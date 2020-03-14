fa0cbe9f1587ac26281c833ef2d34c36d044ac7e
Alexander Moerman <alexander.moerman@gmail.com>
2020-03-14 13:33:47 +0100

    Bump version to 6.0.0

--- 04 - nuget publish.cmd
+++ 04 - nuget publish.cmd
@@ -1,3 +1,3 @@
-dotnet nuget push "HtmlBuilders/bin/Release/HtmlBuilders.5.0.0.nupkg" -s https://api.nuget.org/v3/index.json
-dotnet nuget push "HtmlBuilders/bin/Release/HtmlBuilders.5.0.0.symbols.nupkg" -s https://nuget.smbsrc.net/
+dotnet nuget push "HtmlBuilders/bin/Release/HtmlBuilders.6.0.0.nupkg" -s https://api.nuget.org/v3/index.json
+dotnet nuget push "HtmlBuilders/bin/Release/HtmlBuilders.6.0.0.symbols.nupkg" -s https://nuget.smbsrc.net/
 pause
